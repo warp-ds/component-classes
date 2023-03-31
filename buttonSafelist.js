91974f900c8cb54a5b4bf1170c8a2e5f06125018
AnnaRybkina <37986637+AnnaRybkina@users.noreply.github.com>
2023-03-31 15:11:10 +0200

    fix: generate css classes (#11)

--- buttonSafelist.js
+++
@@ -1,4 +0,0 @@
-
-import { button } from './index.js';
-
-export const buttonSafelist = [...new Set(Object.values(button).map(v => v.split(/\s/)).flat())];
---
+++ vite.config.js
@@ -0,0 +1,25 @@
+/* eslint-env node */
+import { presetWarp } from '@warp-ds/uno'
+import uno from 'unocss/vite'
+import { classes } from './classes.js';
+
+export default () => {
+  return {
+    plugins: [
+      uno({
+        presets: [presetWarp()],
+        mode: 'dist-chunk',
+        safelist: classes,
+      }),  
+
+    ],
+    build: {
+        emptyOutDir: false,
+        rollupOptions: {
+            output: {
+              assetFileNames: `assets/common.[ext]`
+            }
+        }
+    }
+  };
+};
