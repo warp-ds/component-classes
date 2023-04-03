b4517fce00c51ffcea58d48841c1cfaa598dd836
BalbinaK <balbuhhha@gmail.com>
2023-04-03 16:25:48 +0200

    Revert: export only internal classes from classes.js
    This reverts commit 1d617d5c6a8afa9247bc914d078159fe573c5c30.

--- classes.js
+++ classes.js
@@ -1,14 +1,8 @@
 import * as components from './index.js';
 
-const filterInternalClasses = (str) => {
-    const classes = str.split(/\s/);
-    return classes.filter(c => c.startsWith("i-"));
-}
-
 export const classes = Object.values(components)
     .map((e) => {
-        if (typeof e === 'object') return Object.values(e).map(filterInternalClasses);
-
-        return filterInternalClasses(e)
+        if (typeof e === 'object') return Object.values(e).map((e) => e.split(/\s/));
+        return e.split(/\s/);
     })
     .flat(Infinity);
