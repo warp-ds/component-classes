b415d696b5e9daa7d6c977f4702cba07ea7caa3f
Anna Rybkina <anna.rybkina@anicode.se>
2023-04-13 11:55:19 +0200

    fix: add optional to label

--- index.js
+++ index.js
@@ -251,7 +251,6 @@ export const alert = {
 
 export const input = {
   default: 'block text-16 mb-0 leading-22 i-text-$color-input-text-filled i-bg-$color-input-background i-border-$color-input-border hover:i-border-$color-input-border-hover active:i-border-$color-input-border-active rounded-4 py-12 px-8 block border-1 w-full focusable',
-  optional: 'pl-8 font-normal text-14 i-text-$color-label-optional-text',
   disabled: 'i-bg-$color-input-background-disabled i-border-$color-input-border-disabled hover:i-border-$color-input-border-disabled! i-text-$color-input-text-disabled pointer-events-none',
   invalid: 'focusable i-border-$color-input-border-error i-text-$color-input-text-error!',
   readOnly: 'pl-0 bg-transparent border-0 pointer-events-none i-text-$color-input-text-read-only',
@@ -263,6 +262,7 @@ export const label = {
     label: 'antialiased block relative text-14 font-bold pb-4 cursor-pointer',
     labelValid: 'i-text-$color-label-text',
     labelInvalid: 'i-text-$color-label-text-negative',
+    optional: 'pl-8 font-normal text-14 i-text-$color-label-optional-text',
 }
 
 export const helpText = {
