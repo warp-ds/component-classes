d63c8ad5fc2849abc99b9c481a40deddef155b3d
CarolineGard <caroline.gard@mpyadigital.com>
2023-05-17 15:18:18 +0200

    feat(gridlayout): add classNames for gridlayout for dynamic classnames

--- index.js
+++ index.js
@@ -202,7 +202,19 @@ export const tab = {
   contentUnderlined: 'content-underlined', // content-underlined is a no-op that prevents a quirk in how Vue handles class bindings
   contentUnderlinedInactive: '', // missing tokens
   contentUnderlinedActive: 'i-text-$color-tabs-text-active',
-  contentContainedActive: '',
+};
+
+// Todo: Handle dynamic classnames
+export const gridLayout = {
+  cols1: 'grid-cols-1',
+  cols2: 'grid-cols-2',
+  cols3: 'grid-cols-3',
+  cols4: 'grid-cols-4',
+  cols5: 'grid-cols-5',
+  cols6: 'grid-cols-6',
+  cols7: 'grid-cols-7',
+  cols8: 'grid-cols-8',
+  cols9: 'grid-cols-9',
 };
 
 export const buttonReset =
