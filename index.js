96999554f4be003656afd7a2c07f503b4cd4e627
Anna Rybkina <anna.rybkina@anicode.se>
2023-05-19 10:32:08 +0200

    fix: fix typo again

--- index.js
+++ index.js
@@ -49,26 +49,6 @@ export const slider = {
     'i-bg-$color-slider-handle-background-disabled cursor-disabled pointer-events-none',
 };
 
-export const modal = {
-  //TODO: this class can be removed when we have the solution for opacity and we can add rgba values to the background of the backdrop
-  transparentBg: `before:i-bg-$color-modal-backdrop-background before:content-[''] before:absolute before:top-0 before:bottom-0 before:left-0 before:right-0 before:opacity-25`,
-  backdrop:
-    'fixed inset-0 flex sm:place-content-center sm:place-items-center items-end z-20',
-  modal:
-    'sm:pb-safe i-shadow-$shadow-modal w-[640px] max-h-4/5 relative transition-300 backface-hidden will-change-height rounded-8 mx-0 sm:mx-16 i-bg-$color-modal-background flex flex-col overflow-hidden outline-none space-y-16 pt-8 sm:pt-32 sm:pb-32 rounded-b-0 sm:rounded-b-8',
-  content:
-    'block overflow-y-auto overflow-x-hidden last:mb-0 flex-grow flex-shrink px-16 sm:px-32 relative',
-  footer: 'flex justify-end flex-shrink-0 px-16 sm:px-32',
-  transitionTitle: 'transition-all duration-300',
-  title:
-    '-mt-4 sm:-mt-8 h-40 sm:h-48 grid f-grid gap-8 sm:gap-16 grid-cols-[auto_1fr_auto] items-center px-16 sm:px-32 border-b sm:border-b-0 flex-shrink-0',
-  titleText: 'mb-0 h4 sm:h3',
-  titleButton: button.buttonPill + 'sm:min-h-[32px] sm:min-w-[32px]',
-  titleButtonLeft: '-ml-8 sm:-ml-12',
-  titleButtonRight: '-mr-8 sm:-mr-12',
-  titleButtonIcon: 'h-16 w-16 sm:h-24 sm:w-24',
-};
-
 export const box = {
   box: 'group block relative break-words last:mb-0 p-16 rounded-8', // Relative here enables w-clickable
   bleed: '-mx-16 sm:mx-0 rounded-l-0 rounded-r-0 sm:rounded-8', // We target L and R to override the default rounded-8
@@ -287,6 +267,26 @@ export const buttonGroupItem = {
   outlinedSelected: 'i-border-$color-buttongroup-utility-border-active',
   selected: 'z-30 i-text-$color-buttongroup-utility-text-active! i-bg-$color-buttongroup-utility-background-active!',
 }
+
+export const modal = {
+  //TODO: this class can be removed when we have the solution for opacity and we can add rgba values to the background of the backdrop
+  transparentBg: `before:i-bg-$color-modal-backdrop-background before:content-[''] before:absolute before:top-0 before:bottom-0 before:left-0 before:right-0 before:opacity-25`,
+  backdrop:
+    'fixed inset-0 flex sm:place-content-center sm:place-items-center items-end z-20',
+  modal:
+    'sm:pb-safe i-shadow-$shadow-modal w-[640px] max-h-4/5 relative transition-300 backface-hidden will-change-height rounded-8 mx-0 sm:mx-16 i-bg-$color-modal-background flex flex-col overflow-hidden outline-none space-y-16 pt-8 sm:pt-32 sm:pb-32 rounded-b-0 sm:rounded-b-8',
+  content:
+    'block overflow-y-auto overflow-x-hidden last:mb-0 flex-grow flex-shrink px-16 sm:px-32 relative',
+  footer: 'flex justify-end flex-shrink-0 px-16 sm:px-32',
+  transitionTitle: 'transition-all duration-300',
+  title:
+    '-mt-4 sm:-mt-8 h-40 sm:h-48 grid f-grid gap-8 sm:gap-16 grid-cols-[auto_1fr_auto] items-center px-16 sm:px-32 border-b sm:border-b-0 flex-shrink-0',
+  titleText: 'mb-0 h4 sm:h3',
+  titleButton: button.buttonPill + 'sm:min-h-[32px] sm:min-w-[32px]',
+  titleButtonLeft: '-ml-8 sm:-ml-12',
+  titleButtonRight: '-mr-8 sm:-mr-12',
+  titleButtonIcon: 'h-16 w-16 sm:h-24 sm:w-24',
+};
 
 export const alert = {
   alert: "flex p-16 border border-l-4 rounded-4",
