da2823e1c674044a16c4c5d6724dcdf333197d23
CarolineGard <caroline.gard@mpyadigital.com>
2023-05-22 09:01:38 +0200

    fix(slider): add shadow for slider

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ export const slider = {
   thumb:
     'absolute transition-shadow w-24 h-24 bottom-10 rounded-4',
   thumbEnabled:
-    'border-2 focusable slider-handle-outline-none shadow-small cursor-pointer i-bg-$color-slider-handle-background i-border-$color-slider-handle-border hover:i-bg-$color-slider-handle-background-hover hover:i-border-$color-slider-handle-border-hover hover:slider-handle-shadow-hover active:i-bg-$color-slider-handle-background-active active:i-border-$color-slider-handle-border-active active:slider-handle-shadow-active focus:slider-handle-shadow-hover focus:i-border-$color-slider-handle-border-hover focus:i-bg-$color-slider-handle-background-hover',
+    'border-2 focusable slider-handle-outline-none i-shadow-$shadow-slider cursor-pointer i-bg-$color-slider-handle-background i-border-$color-slider-handle-border hover:i-bg-$color-slider-handle-background-hover hover:i-border-$color-slider-handle-border-hover hover:slider-handle-shadow-hover active:i-bg-$color-slider-handle-background-active active:i-border-$color-slider-handle-border-active active:slider-handle-shadow-active focus:slider-handle-shadow-hover focus:i-border-$color-slider-handle-border-hover focus:i-bg-$color-slider-handle-background-hover',
   thumbDisabled:
     'i-bg-$color-slider-handle-background-disabled cursor-disabled pointer-events-none',
 };
