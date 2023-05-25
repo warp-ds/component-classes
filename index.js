4df8112b90c66f35faa248c2c19ee7b895765051
Magnus Hübner <magnus.hubner@aptitud.se>
2023-05-25 14:47:42 +0200

    fix: transition-gpu -> transform-gpu

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ export const switchToggle = {
   trackActive: 'i-bg-$color-switch-track-background-active group-hover:i-bg-$color-switch-track-background-active-hover',
   trackInactive: 'i-bg-$color-switch-track-background group-hover:i-bg-$color-switch-track-background-hover',
   trackDisabled: 'i-bg-$color-switch-track-background-disabled',
-  handle: 'absolute transition-gpu h-16 w-16 top-4 left-4 rounded-full transition-transform',
+  handle: 'absolute transform-gpu h-16 w-16 top-4 left-4 rounded-full transition-transform',
   handleSelected: 'translate-x-20',
   handleNotDisabled: 'i-bg-$color-switch-handle-background shadow-small',
   handleDisabled: 'i-bg-$color-switch-handle-background-disabled',
