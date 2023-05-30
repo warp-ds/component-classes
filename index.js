4b4af695dd7a915690684cb486ea8ddff46e7d89
Magnus Hübner <magnus.hubner@aptitud.se>
2023-05-30 13:28:01 +0200

    fix: move react specific switch track classes to component classes

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ export const switchToggle = {
   switch: 'tap-highlight-transparent',
   label: 'block relative h-24 w-44 cursor-pointer group',
   labelDisabled: 'pointer-events-none',
-  track: 'absolute h-full w-full rounded-full transition-colors',
+  track: 'absolute top-0 left-0 h-full w-full rounded-full transition-colors',
   trackActive: 'i-bg-$color-switch-track-background-active group-hover:i-bg-$color-switch-track-background-active-hover',
   trackInactive: 'i-bg-$color-switch-track-background group-hover:i-bg-$color-switch-track-background-hover',
   trackDisabled: 'i-bg-$color-switch-track-background-disabled',
