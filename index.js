fb87898cf2c4d96fbbe4366fef0c989196e08014
CarolineGard <caroline.gard@mpyadigital.com>
2023-06-01 11:44:15 +0200

    feat(ribbon): update with warp tokens

--- index.js
+++ index.js
@@ -25,15 +25,16 @@ export const pageIndicator = {
 };
 
 export const ribbon = {
-  base: 'py-4 px-8 border rounded-4 inline-flex last-child:mb-0',
-  info: 'border-aqua-50 bg-aqua-50',
-  success: 'border-green-100 bg-green-100',
-  warning: 'border-yellow-100 bg-yellow-100',
-  error: 'border-red-100 bg-red-100',
-  disabled: 'border-bluegray-300 bg-bluegray-300',
-  sponsored: 'border-aqua-200 bg-aqua-200',
-  neutral: 'border-gray-100 bg-gray-100',
-  outlined: 'border-bluegray-300 bg-white',
+  base: 'py-4 px-8 border rounded-4 inline-flex last:mb-0',
+  info: ' i-bg-$color-badge-info-background i-text-$color-badge-info-text',
+  success: ' i-bg-$color-badge-positive-background i-text-$color-badge-positive-text',
+  warning: ' i-bg-$color-badge-warning-background i-text-$color-badge-warning-text',
+  error: ' i-bg-$color-badge-negative-background i-text-$color-badge-negative-text',
+  disabled: 'i-border-$color-badge-disabled-background i-bg-$color-badge-disabled-background i-text-$color-badge-disabled-text',
+  sponsored: 'i-border-$color-badge-price-background i-bg-$color-badge-price-background i-text-$color-badge-price-text',
+  neutral: 'i-border-$color-badge-neutral-background i-bg-$color-badge-neutral-background i-text-$color-badge-neutral-text',
+  roundedTopRightBottomLeft: 'rounded-tr-0 rounded-bl-0',
+  roundedTopLeftBottomRight: 'rounded-tl-0 rounded-br-0',
 };
 
 export const slider = {
