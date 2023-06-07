1365ce2c52af8638bbe07398bad06303b6551352
Caroline Gard <caroline.gard@mpyadigital.com>
2023-06-07 14:21:30 +0200

    fix(step): move classes to component-classes + cleanup of unused classes (#76)

--- index.js
+++ index.js
@@ -86,18 +86,13 @@ export const step = {
   stepHorizontal: 'group/steph grid-rows-[auto_20px] grid-cols-[1fr_20px_1fr] flex-1 grid gap-y-16 items-center',
 
   stepDot: 'rounded-full border-2 h-20 w-20 transition-colors duration-300 i-text-$color-stepindicator-handle-icon',
-  stepDotVertical: '',
-  stepDotVerticalLeft: '',
   stepDotVerticalRight: 'col-start-2',
   stepDotHorizontal: 'row-start-2 justify-self-end',
-
   stepDotActive: 'i-border-$color-stepindicator-handle-border-active i-bg-$color-stepindicator-handle-background-active',
-  stepDotComplete: 'i-border-$color-stepindicator-handle-border-active i-bg-$color-stepindicator-handle-background-active',
   stepDotIncomplete: 'i-border-$color-stepindicator-handle-border i-bg-$color-stepindicator-handle-background',
 
   stepLine: 'group-last/stepv:hidden transition-colors duration-300',
   stepLineVertical: 'w-2 h-full justify-self-center',
-  stepLineVerticalLeft: '',
   stepLineVerticalRight: 'col-start-2',
   stepLineHorizontal: 'h-2 w-full row-start-2',
   stepLineHorizontalRight: 'group-last/steph:bg-transparent',
@@ -110,6 +105,11 @@ export const step = {
   contentVertical: 'row-span-2 pb-32',
   contentHorizontal: 'col-span-3 px-16 row-start-1 text-center',
 };
+
+export const steps = {
+  steps: 'w-full',
+  stepsHorizontal: 'flex',
+}
 
 export const card = {
   card: 'cursor-pointer overflow-hidden relative transition-all',
