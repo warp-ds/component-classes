e5c8d761943806b59f5e3b8e24d2152772b3a32e
Caroline Gard <caroline.gard@mpyadigital.com>
2023-06-19 17:51:50 +0200

    fix(ribbon/tag): update ribbon until we have correct border colors (#92)

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ export const pageIndicator = {
 
 export const ribbon = {
   base: 'py-4 px-8 border rounded-4 inline-flex last:mb-0',
-  info: 'i-bg-$color-badge-info-background i-text-$color-badge-info-text',
-  success: 'i-bg-$color-badge-positive-background i-text-$color-badge-positive-text',
-  warning: 'i-bg-$color-badge-warning-background i-text-$color-badge-warning-text',
-  error: ' i-bg-$color-badge-negative-background i-text-$color-badge-negative-text',
+  info: 'i-border-$color-badge-info-background i-bg-$color-badge-info-background i-text-$color-badge-info-text',
+  success: 'i-border-$color-badge-positive-background i-bg-$color-badge-positive-background i-text-$color-badge-positive-text',
+  warning: 'i-border-$color-badge-warning-background i-bg-$color-badge-warning-background i-text-$color-badge-warning-text',
+  error: 'i-border-$color-badge-negative-background i-bg-$color-badge-negative-background i-text-$color-badge-negative-text',
   disabled: 'i-border-$color-badge-disabled-background i-bg-$color-badge-disabled-background i-text-$color-badge-disabled-text',
   sponsored: 'i-border-$color-badge-price-background i-bg-$color-badge-price-background i-text-$color-badge-price-text',
   neutral: 'i-border-$color-badge-neutral-background i-bg-$color-badge-neutral-background i-text-$color-badge-neutral-text',
